564811e5013e1c97387415286b29eec4fb9b7b78
rohan <rsuri@irusmail.com>
2024-01-02 01:17:39 -0500

    re-ordered contexts & renamed Initialize

--- programs/token_vesting/src/lib.rs
+++ programs/token_vesting/src/lib.rs
@@ -8,7 +8,7 @@ pub mod token_vesting {
 
     use super::*;
 
-    pub fn initialize(ctx: Context<InitializeNewVest>, beneficiaries: Vec<Beneficiary>, amount: u64, decimals: u8, data_bump: u8, _escrow_bump: u8) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, beneficiaries: Vec<Beneficiary>, amount: u64, decimals: u8, data_bump: u8, _escrow_bump: u8) -> Result<()> {
         let data_account = &mut ctx.accounts.data_account;
         data_account.beneficiaries = beneficiaries;
         data_account.percent_available = 0;
@@ -88,58 +88,7 @@ pub mod token_vesting {
 }
 
 #[derive(Accounts)]
-#[instruction(data_bump: u8, wallet_bump: u8)]
-pub struct Claim<'info> {
-    #[account(
-        mut,
-        seeds = [b"data_account", token_mint.key().as_ref()], 
-        bump = data_bump,
-    )]
-    pub data_account: Account<'info, DataAccount>,
-    
-    #[account(
-        mut,
-        seeds=[b"escrow_wallet".as_ref(), token_mint.key().as_ref()],
-        bump = wallet_bump,
-    )]
-    escrow_wallet: Account<'info, TokenAccount>,
-
-    #[account(mut)]
-    pub sender: Signer<'info>,
-    
-    pub token_mint: Account<'info, Mint>,
-
-    #[account(
-        init_if_needed,
-        payer = sender,
-        associated_token::mint = token_mint,
-        associated_token::authority = sender,
-    )]
-    pub wallet_to_deposit_to: Account<'info, TokenAccount>,
-
-    pub associated_token_program: Program<'info, AssociatedToken>, // Don't actually use it in the instruction, but used for the wallet_to_deposit_to account
-    pub token_program: Program<'info, Token>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-#[instruction(data_bump: u8)]
-pub struct Release<'info> {
-    #[account(
-        mut,
-        seeds = [b"data_account", token_mint.key().as_ref()], 
-        bump = data_bump,
-    )]
-    pub data_account: Account<'info, DataAccount>,
-    pub token_mint: Account<'info, Mint>,
-
-    #[account(mut)]
-    pub sender: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct InitializeNewVest<'info> {
+pub struct Initialize<'info> {
     #[account(
         init,
         payer = sender,
@@ -170,8 +119,65 @@ pub struct InitializeNewVest<'info> {
 
     #[account(mut)]
     pub sender: Signer<'info>,
+    
     pub system_program: Program<'info, System>,
+    
     pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+#[instruction(data_bump: u8)]
+pub struct Release<'info> {
+    #[account(
+        mut,
+        seeds = [b"data_account", token_mint.key().as_ref()], 
+        bump = data_bump,
+    )]
+    pub data_account: Account<'info, DataAccount>,
+    
+    pub token_mint: Account<'info, Mint>,
+
+    #[account(mut)]
+    pub sender: Signer<'info>,
+    
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(data_bump: u8, wallet_bump: u8)]
+pub struct Claim<'info> {
+    #[account(
+        mut,
+        seeds = [b"data_account", token_mint.key().as_ref()], 
+        bump = data_bump,
+    )]
+    pub data_account: Account<'info, DataAccount>,
+    
+    #[account(
+        mut,
+        seeds=[b"escrow_wallet".as_ref(), token_mint.key().as_ref()],
+        bump = wallet_bump,
+    )]
+    escrow_wallet: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub sender: Signer<'info>,
+    
+    pub token_mint: Account<'info, Mint>,
+
+    #[account(
+        init_if_needed,
+        payer = sender,
+        associated_token::mint = token_mint,
+        associated_token::authority = sender,
+    )]
+    pub wallet_to_deposit_to: Account<'info, TokenAccount>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>, // Don't actually use it in the instruction, but used for the wallet_to_deposit_to account
+    
+    pub token_program: Program<'info, Token>,
+    
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Default, Copy, Clone, AnchorSerialize, AnchorDeserialize)]
